fa4b70b54ca964f0ab1847482a91d1262ac1a84c
woojunchoi <tim.woojune1@gmail.com>
2018-03-24 14:12:12 -0700

    now we are able to send data to server / socket

--- public/chat.js
+++ public/chat.js
@@ -1,1 +1,19 @@
 const socket = io.connect('http://localhost:3000')
+
+let message = document.getElementById('message')
+let handle = document.getElementById('handle')
+let output = document.getElementsByClassName('output')[0]
+let btn = document.getElementById('send')
+
+//emit events
+btn.addEventListener('click', () => {
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    })
+    message.value='';
+})
+
+socket.on('chat',(data) =>{
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+})
