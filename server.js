dc1dc2c0da4e87c0b7eab796ec3595e75ddefc09
woojunchoi <tim.woojune1@gmail.com>
2018-03-24 01:50:31 -0700

    making connection with front end

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express();
+const socket = require('socket.io')
 
-app.listen(3000, () => {
+//local server setup
+const server = app.listen(3000, () => {
     console.log('connected to server')
 })
 
+//serve static files 
 app.use(express.static(__dirname+'/public'))
+
+const io = socket(server);
+io.on('connection', (socket) => {
+    console.log('socket connected')
+})
